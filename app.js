3636b0896a53ab4262742d8960d97043dfd59530
Iyare Usen <iyare.usen@gmail.com>
2025-02-10 08:27:23 +0100

    still debugging final bug

--- app.js
+++ app.js
@@ -25,139 +25,148 @@ const deleteBtn = document.querySelector("#delete");
 const inputDisplay = document.querySelector("#inputs");
 const resultDisplay = document.querySelector("#results");
 let subTotal = "";
-
-
-
+let currentInput = "";
 
 // Add evenetlisteners
 
-one.addEventListener("click", function() {
+one.addEventListener("click", function () {
     inputDisplay.textContent += "1";
 });
-two.addEventListener("click", function() {
-    inputDisplay.textContent += "2";
+two.addEventListener("click", function () {
+  inputDisplay.textContent += "2";
 });
-three.addEventListener("click", function() {
-    inputDisplay.textContent += "3";
+three.addEventListener("click", function () {
+  inputDisplay.textContent += "3";
 });
-four.addEventListener("click", function() {
-    inputDisplay.textContent += "4";
+four.addEventListener("click", function () {
+  inputDisplay.textContent += "4";
 });
-five.addEventListener("click", function() {
-    inputDisplay.textContent += "5";
+five.addEventListener("click", function () {
+  inputDisplay.textContent += "5";
 });
-six.addEventListener("click", function() {
-    inputDisplay.textContent += "6";
+six.addEventListener("click", function () {
+  inputDisplay.textContent += "6";
 });
-seven.addEventListener("click", function() {
-    inputDisplay.textContent += "7";
+seven.addEventListener("click", function () {
+  inputDisplay.textContent += "7";
 });
-eight.addEventListener("click", function() {
-    inputDisplay.textContent += "8";
+eight.addEventListener("click", function () {
+  inputDisplay.textContent += "8";
 });
-nine.addEventListener("click", function() {
-    inputDisplay.textContent += "9";
+nine.addEventListener("click", function () {
+  inputDisplay.textContent += "9";
 });
-zero.addEventListener("click", function() {
-    inputDisplay.textContent += "0";
+zero.addEventListener("click", function () {
+  inputDisplay.textContent += "0";
 });
 add.addEventListener("click", function () {
-    let operator = add.value;
-    checkTotal();
-    checkOperator(operator);
-    inputDisplay.textContent += "+";
+  checkOperator();
+  checkTotal();
+  inputDisplay.textContent += "+";
 });
 subtract.addEventListener("click", function () {
-    let operator = subtract.value;
-    checkTotal();
-    checkOperator(operator);
-    inputDisplay.textContent += "-";
-    
+  checkTotal();
+  checkOperator();
+  inputDisplay.textContent += "-";
 });
 multiply.addEventListener("click", function () {
-    let operator = multiply.value;
-    checkTotal();
-
-    checkOperator(operator);
-    inputDisplay.textContent += "*";
+  checkTotal();
+  checkOperator();
+  inputDisplay.textContent += "*";
 });
 
 divide.addEventListener("click", function () {
-    let operator = divide.value;
-    checkTotal();
-    inputDisplay.textContent += "/";  
+  checkTotal();
+  checkOperator();
+  inputDisplay.textContent += "/";
 });
-decimal.addEventListener("click", function() {
-    inputDisplay.textContent += ".";
+decimal.addEventListener("click", function () {
+  inputDisplay.textContent += ".";
 });
 
 percent.addEventListener("click", function () {
-    inputDisplay.textContent += "%";
-    let percentIndex = inputDisplay.textContent.indexOf("%");
-    // Get the last operator before the percentage sign
-    let lastOperator = inputDisplay.textContent.slice(0, percentIndex).match(/[\+\-\*\/](?!.*[\+\-\*\/])/);
-    let percentValue = "";
-    // Slice the string from the beginning to the percentage sign
-    if (lastOperator === null) {
-        percentValue = inputDisplay.textContent.slice(0, percentIndex);
-    } else {
-        percentValue = inputDisplay.textContent.slice(inputDisplay.textContent.lastIndexOf(lastOperator[0]) + 1, percentIndex);
-    }
-    // Convert the percentage value to a decimal
-    let percentResult = percentValue / 100;
-    
-    // Replace the percentage value with the decimal value
-    inputDisplay.textContent = inputDisplay.textContent.replace(percentValue + "%", percentResult);
+  inputDisplay.textContent += "%";
+  let percentIndex = inputDisplay.textContent.indexOf("%");
+  // Get the last operator before the percentage sign
+  let lastOperator = inputDisplay.textContent
+    .slice(0, percentIndex)
+    .match(/[\+\-\*\/](?!.*[\+\-\*\/])/);
+  let percentValue = "";
+  // Slice the string from the beginning to the percentage sign
+  if (lastOperator === null) {
+    percentValue = inputDisplay.textContent.slice(0, percentIndex);
+  } else {
+    percentValue = inputDisplay.textContent.slice(
+      inputDisplay.textContent.lastIndexOf(lastOperator[0]) + 1,
+      percentIndex
+    );
+  }
+  // Convert the percentage value to a decimal
+  let percentResult = percentValue / 100;
+
+  // Replace the percentage value with the decimal value
+  inputDisplay.textContent = inputDisplay.textContent.replace(
+    percentValue + "%",
+    percentResult
+  );
 });
 
-clear.addEventListener("click", function() {
-    inputDisplay.textContent = "";
-    resultDisplay.textContent = "";
-    subTotal = "";
+clear.addEventListener("click", function () {
+  inputDisplay.textContent = "";
+  resultDisplay.textContent = "";
+  subTotal = "";
 });
 
 equals.addEventListener("click", function () {
-    const myEval = eval;
-    let result = evaluate()
-    if (result === Infinity || result === NaN || result === -Infinity) {
-        resultDisplay.textContent = "Cannot divide by zero";
-        subTotal = "0";
-    } else {
-        resultDisplay.textContent = result
-        subTotal = myEval(inputDisplay.textContent);
-    }
+  let result = evaluate();
+  if (result === Infinity || result === NaN || result === -Infinity) {
+    resultDisplay.textContent = "Cannot divide by zero";
+    subTotal = "0";
+  } else {
+    resultDisplay.textContent = result;
+    subTotal = result;
+  }
 });
 
-deleteBtn.addEventListener("click", function() {
-    inputDisplay.textContent = inputDisplay.textContent.slice(0, -1);
-
+deleteBtn.addEventListener("click", function () {
+  inputDisplay.textContent = inputDisplay.textContent.slice(0, -1);
 });
 
 function evaluate() {
-      return eval(inputDisplay.textContent);
+  const myEval = eval;
+  return myEval(inputDisplay.textContent);
 }
 
 function checkTotal() {
-    if (resultDisplay.textContent !== "" && resultDisplay.textContent !== "Cannot divide by zero") {
-        subTotal = resultDisplay.textContent;
-        inputDisplay.textContent = subTotal;
-    } else {
-        resultDisplay.textContent = "";
-        subTotal = inputDisplay.textContent;
-    }
-}
-function getLastChar() { 
-    let lastChar = inputDisplay.textContent.slice(-1);
-    return lastChar;
+  if (
+    resultDisplay.textContent !== "" &&
+    resultDisplay.textContent !== "Cannot divide by zero" &&
+    !isNaN(resultDisplay.textContent)
+  ) {
+    subTotal = resultDisplay.textContent;
+    inputDisplay.textContent = subTotal;
+  } else {
+    resultDisplay.textContent = "";
+    
+  }
 }
 
-function checkOperator(operator) {
-    let lastChar = getLastChar()
+function getCurrentInput() {
+  currentInput = inputDisplay.textContent;
+  return currentInput;
+}
+function checkOperator() {
+  let lastChar = inputDisplay.textContent.slice(-1);
     console.log(lastChar);
-    if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
-        inputDisplay.textContent = inputDisplay.textContent.slice(0, -1);
-        console.log("last character is an operator");
+    if (
+      lastChar === "+" ||
+      lastChar === "-" ||
+      lastChar === "*" ||
+      lastChar === "/"
+    ) {
+      inputDisplay.textContent = inputDisplay.textContent.slice(0, -1);
+      console.log("last character is an operator");
     } else {
-        console.log("can't identify last character");
+      console.log("last character is not an operator");
     }
 }
